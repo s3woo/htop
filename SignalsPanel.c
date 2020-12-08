672caacd7403375ec42133736d9e0250938825ed
s3woo <73628875+s3woo@users.noreply.github.com>
2020-12-08 17:20:00 +0900

    limitpanel

--- SignalsPanel.c
+++ SignalsPanel.c
@@ -1,21 +1,22 @@
 /*
 htop - SignalsPanel.c
 (C) 2004-2011 Hisham H. Muhammad
-Released under the GNU GPLv2, see the COPYING file
+Released under the GNU GPL, see the COPYING file
 in the source distribution for its full text.
 */
 
-#include "SignalsPanel.h"
-
-#include <signal.h>
-#include <stdbool.h>
-
-#include "FunctionBar.h"
-#include "ListItem.h"
-#include "Object.h"
 #include "Panel.h"
+#include "SignalsPanel.h"
 #include "Platform.h"
-#include "XUtils.h"
+
+#include "ListItem.h"
+#include "RichString.h"
+
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+
+#include <ctype.h>
 
 
 Panel* SignalsPanel_new() {
@@ -35,7 +36,7 @@ Panel* SignalsPanel_new() {
       static char buf[16];
       for (int sig = SIGRTMIN; sig <= SIGRTMAX; i++, sig++) {
          int n = sig - SIGRTMIN;
-         xSnprintf(buf, sizeof(buf), "%2d SIGRTMIN%-+3d", sig, n);
+         xSnprintf(buf, 16, "%2d SIGRTMIN%-+3d", sig, n);
          if (n == 0) {
             buf[11] = '\0';
          }
@@ -47,3 +48,22 @@ Panel* SignalsPanel_new() {
    Panel_setSelected(this, defaultPosition);
    return this;
 }
+
+Panel* LimitPanel_new() {
+   Panel* this = Panel_new(1, 1, 1, 1, true, Class(ListItem), FunctionBar_newEnterEsc("Send   ", "Cancel "));
+   const int defaultLimit = 50;
+   int defaultPosition = 50;
+   unsigned int i;
+   for (i = 0; i < 100; i++) {
+      Panel_set(this, i, (Object*) ListItem_new(Platform_limit[i].name, Platform_limit[i].number));
+      // signal 15 is not always the 15th signal in the table
+      if (Platform_limit[i].number == defaultLimit) {
+         defaultPosition = i;
+      }
+   }
+  
+   Panel_setHeader(this, "Send Limit(%):");
+   Panel_setSelected(this, defaultPosition);
+   return this;
+}
+
